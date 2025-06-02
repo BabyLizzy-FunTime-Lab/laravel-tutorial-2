8ddc3cfc84802e6e6321449e1418f18a5df0dedf
Darthporkius <cholochullo@hotmail.com>
2025-06-02 16:28:52 +0200

    added Login and the navbar shows content selectively

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -18,16 +18,22 @@
     <header>
         <nav>
             <h1>Ninja Network</h1>
-            <a href="{{ route('ninjas.index') }}">All Ninjas</a>
-            <a href="{{ route('ninjas.create') }}">Create a Ninja</a>
+            @guest
+                <a href="{{ route('show.login') }}" class="btn">Login</a>
+                <a href="{{ route('show.register') }}" class="btn">Register</a>
+            @endguest
 
-            <a href="{{ route('show.login') }}" class="btn">Login</a>
-            <a href="{{ route('show.register') }}" class="btn">Register</a>
-
-            <form action="{{ route('logout') }}" method="POST" class="m-0">
-                @csrf
-                <button class="btn">Logout</button>
-            </form>
+            @auth
+                <span class="border-r-2 pr-2">
+                    Hi there, {{ Auth::user()->name }}
+                </span>
+                <a href="{{ route('ninjas.index') }}">All Ninjas</a>
+                <a href="{{ route('ninjas.create') }}">Create a Ninja</a>
+                <form action="{{ route('logout') }}" method="POST" class="m-0">
+                    @csrf
+                    <button class="btn">Logout</button>
+                </form>
+            @endauth
         </nav>
     </header>
     <main class="container">
